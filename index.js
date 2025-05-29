9830d5007e011b8a45fa5d28b47fe1a27e626f51
Cassandra <cassandra.moore1516@gmail.com>
2025-05-28 20:22:28 -0500

    Created index.js file and console.log

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Welcome Cassandra Moore to Node.js!");
+console.log("The current date and time is:", new Date());
+console.log("Welcome to Node.js!");
+console.log("On May 31, 2025 Saturday is Your Birthday");
+console.log("Happy Birthday Cassandra Moore");
+console.log("Your local time is:", new Date().toLocaleString());
