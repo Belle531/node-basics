1d8cab27aa37c869a203b3cec0c76e5a5f1faec6
Cassandra <cassandra.moore1516@gmail.com>
2025-05-28 20:30:00 -0500

    Added new consloe.log Day 1 Assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("Welcome Cassandra Moore to Node.js!");
 console.log("The current date and time is:", new Date());
 console.log("Welcome to Node.js!");
+console.log("Day 1 Assignment")
 console.log("On May 31, 2025 Saturday is Your Birthday");
 console.log("Happy Birthday Cassandra Moore");
 console.log("Your local time is:", new Date().toLocaleString());
